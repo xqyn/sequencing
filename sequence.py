58d1cb79a18969482ebd0bca50da4ccffc0e071d
xqyn <nxquy.bms@gmail.com>
2025-08-01 11:40:58 +0200

    move belt and merge dna to seq

--- sequence.py
+++ sequence.py
@@ -3,12 +3,31 @@ project: sequencing
 XQ - Leiden UMC
 modifying func for dna sequence / dna list
 update: 2025
+    - march 14: add dna
     - april 22: compute_base_count
     - mei 2: hamming_dist
 """
 
 from typing import NamedTuple, List, Dict
 from itertools import zip_longest
+
+# --- dna: modifying func for dna sequence
+def ascii_to_phred(quality_string, offset=33):
+    """Convert an ASCII quality to Phred scores"""
+    return [ord(char) - offset for char in quality_string]
+
+
+def complement(seq, reverse=False):
+    """Return the complement of a sequence"""
+    mapping = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C',
+               'a': 't', 't': 'a', 'c': 'g', 'g': 'c'}
+    complemented = ''.join([mapping.get(base, base) for base in seq])
+    return complemented[::-1] if reverse else complemented
+
+
+def rever_complement(seq):
+    """Return the reverse complement of a sequence"""
+    return complement(seq, reverse=True)
 
 
 # --- hamming_dist
